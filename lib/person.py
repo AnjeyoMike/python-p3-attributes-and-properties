de8c62d5b0e5ba84e142f7458f12dfda929b077e
Anjeyo Mike <mike.matekeya@student.moringaschool.com>
2024-07-05 11:24:11 +0300

    Work done

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,15 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="", job=""):
+        if not isinstance(name, str) or len(name.strip()) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name.strip().title()
+
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self.job = job
+
+
